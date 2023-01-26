7e41be8de63e26c96b0c9bc17e568045bd4decb0
centie22 <vicentiapeace@gmail.com>
2023-01-26 08:48:40 +0100

    stake contract

--- src/NFTstake.sol
+++ src/NFTstake.sol
@@ -1,7 +1,64 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.7;
+import "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
 
 contract NFTstake {
+    ERC20 skyToken;
+    ERC721 skyNFT;
+    //ERC721 NFT2;
+    //ERC721 NFT3;
+    address owner;
+
+    struct Staker{
+        address staker;
+        uint24[] tokenId;
+        uint40 timeOfStaking;
+        uint24 currentRewards;
+    }
+
+    uint8 rewardperWeek;
+    uint8 minStakingperiod;
+    mapping (address => Staker) stakerDetails;
+
+    constructor (ERC721 _skyNFT, ERC20 _skyToken
+    //ERC721 _nft2, ERC721 _nft3
+    ){
+        owner = msg.sender;
+        skyNFT = _skyNFT;
+        skyToken = _skyToken;
+        //NFT2 = _nft2;
+        //NFT3 = _nft3;
+    }
+
+    function stake(skyNFT _tokenId) external {
+            Staker storage s = stakerDetails[msg.sender];
+            transferFrom(msg.sender, address(this), _tokenId);
+            s.stake = msg.sender;
+            s.tokenId = _tokenId;
+            s.timeOfStaking = block.timestamp;
+    }
+
+    function claimRewards() external{
+        Staker storage s = stakerDetails[msg.sender];
+        uint40 checkStakePeriod = s.timeOfStaking + block.timestamp;
+        require(checkStakePeriod >= minStakingperiod, "Your staking period is not up to the set minimum staking period.");
+        uint rewards = rewardperWeek * checkStakePeriod;
+        
+    }
+
+    function setRewardPerWeek(skyToken numberOfTokens) external ownerRestriction {
+        rewardperWeek = numberOfTokens * 10*decimal()/ 1 weeks;
+    }
+
+    function setMinStakingPeriod (uint8 _minStakePeriod) external ownerRestriction{
+        minStakingperiod = _minStakePeriod;
+    }
     
-    
+    modifier ownerRestriction(){
+        require(msg.sender == owner, "Function access restricted to contract owner");
+
+        _;
+    }
 }
+//nft development advanced tutorials
--- src/skyNFT.sol
+++ src/skyNFT.sol
@@ -6,22 +6,32 @@ import "lib/openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721URISt
 import "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 import "lib/openzeppelin-contracts/contracts/utils/Counters.sol";
 
-contract MyToken is ERC721, ERC721URIStorage, Ownable {
+contract SkyNFT is ERC721, ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
+    string private _URI = "https://ipfs.filebase.io/ipfs/QmT7VQxnYV2hyF4pYDLpepoua4ox3YqW6Wr7ZwCKckpBiL";
+    uint24 mintAmount;
+    uint16 maxMintAmount;
+    address owner;
+    
 
-    constructor() ERC721("MyToken", "MTK") {}
-
-    function _baseURI() internal pure override returns (string memory) {
-        return "https://ipfs.filebase.io/ipfs/QmT7VQxnYV2hyF4pYDLpepoua4ox3YqW6Wr7ZwCKckpBiL";
+    constructor(uint16 _maxMintAmount) ERC721("skyNFT", "skyNFT") {
+        maxMintAmount = _maxMintAmount;
+        owner = msg.sender;
     }
 
-    function safeMint(address to, string memory uri) public onlyOwner {
+    function _baseURI() internal pure override returns (string memory) {
+        return _URI;
+    }
+
+    function safeMint(address to) public payable{
+        require(_tokenIdCounter.current < maxMintAmount, "Sorry, max mint reached.");
+        assert(msg.value == mintAmount);
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
+        _setTokenURI(tokenId, _URI);
     }
 
     // The following functions are overrides required by Solidity.
@@ -38,4 +48,13 @@ contract MyToken is ERC721, ERC721URIStorage, Ownable {
     {
         return super.tokenURI(tokenId);
     }
-}
+    
+    function setmintAmount (uint24 _Amount) onlyOwner external{
+        mintAmount = _Amount;
+    }
+
+    modifier onlyOwner(){
+        require(msg.sender == owner, "Only contract owner can call this function.");
+        _;
+    }
+ }

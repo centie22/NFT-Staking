d8f9f6e5466ab8a52e3518713464826ce5d03873
MACBOOK AIR <macbookair@MACBOOKs-MacBook-Pro.local>
2023-02-06 11:05:07 -0800

    new changes to staking contract

--- src/NFTstake.sol
+++ src/NFTstake.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.8.7;
 import "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import "lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import "lib/openzeppelin-contracts/contracts/token/ERC721/IERC721Receiver.sol";
 
-contract NFTstake {
+contract NFTstake is IERC721Receiver {
     ERC20 skyToken;
     ERC721 skyNFT;
     //ERC721 NFT2;
@@ -12,9 +13,12 @@ contract NFTstake {
 
     struct Staker{
         address staker;
-        uint24[] tokenId;
+        uint24 tokenId;
         uint40 timeOfStaking;
         uint24 currentRewards;
+        uint40 lastTimeClaimed;
+        bool claimedFirstReward;
+
     }
 
     uint8 rewardperWeek;
@@ -33,7 +37,7 @@ contract NFTstake {
 
     function stake(skyNFT _tokenId) external {
             Staker storage s = stakerDetails[msg.sender];
-            transferFrom(msg.sender, address(this), _tokenId);
+            safeTransferFrom(msg.sender, address(this), _tokenId);
             s.stake = msg.sender;
             s.tokenId = _tokenId;
             s.timeOfStaking = block.timestamp;
@@ -41,14 +45,64 @@ contract NFTstake {
 
     function claimRewards() external{
         Staker storage s = stakerDetails[msg.sender];
-        uint40 checkStakePeriod = s.timeOfStaking + block.timestamp;
+        if (s.claimedFirstReward == false) {
+            
+        uint40 checkStakePeriod = block.timestamp / s.timeOfStaking;
         require(checkStakePeriod >= minStakingperiod, "Your staking period is not up to the set minimum staking period.");
         uint rewards = rewardperWeek * checkStakePeriod;
+        s.claimedFirstReward = true;
+        s.lastTimeClaimed = block.timestamp;
+        skyToken.mint(msg.sender, rewards);
+
+        } else {    
+        uint40 checkStakePeriod = block.timestamp / s.lastTimeClaimed;
+        require(checkStakePeriod >= minStakingperiod, "Your staking period is not up to the set minimum staking period.");
+        uint rewards = rewardperWeek * checkStakePeriod;
+        s.lastTimeClaimed = block.timestamp;
+        skyToken.mint(msg.sender, rewards);
+        }
          
     }
 
+    function claimRewardsAndNFT() external {
+        Staker storage s = stakerDetails[msg.sender];
+        if (s.claimedFirstReward == false) {
+            
+        uint40 checkStakePeriod = block.timestamp / s.timeOfStaking;
+        require(checkStakePeriod >= minStakingperiod, "Your staking period is not up to the set minimum staking period.");
+        uint rewards = rewardperWeek * checkStakePeriod;
+        s.claimedFirstReward = true;
+        s.lastTimeClaimed = block.timestamp;
+        skyNFT.transferFrom(address(this), msg.sender, s.tokenId);
+        skyToken.mint(msg.sender, rewards);
+        delete stakerDetails[msg.sender];
+
+        } else {    
+        uint40 checkStakePeriod = block.timestamp / s.lastTimeClaimed;
+        require(checkStakePeriod >= minStakingperiod, "Your staking period is not up to the set minimum staking period.");
+        uint rewards = rewardperWeek * checkStakePeriod;
+        s.lastTimeClaimed = block.timestamp;
+        skyNFT.transferFrom(address(this), msg.sender, s.tokenId);
+        skyToken.mint(msg.sender, rewards);
+        delete stakerDetails[msg.sender];
+        }
+    }
+
+    function checkTotalRewards() external returns (uint256){
+        Staker storage s = stakerDetails[msg.sender];
+        if (s.claimedFirstReward == false) {
+            uint40 stakePeriod = block.timestamp / s.timeOfStaking;
+            uint rewards = stakePeriod * rewardperWeek;
+            return rewards;
+        } else {
+            uint40 stakePeriod = block.timestamp / s.lastTimeClaimed;
+            uint rewards = stakePeriod * rewardperWeek;
+            return rewards;
+        }
+    }
+
     function setRewardPerWeek(skyToken numberOfTokens) external ownerRestriction {
-        rewardperWeek = numberOfTokens * 10*decimal()/ 1 weeks;
+        rewardperWeek = numberOfTokens * 10 ** decimal()/ 1 weeks;
     }
 
     function setMinStakingPeriod (uint8 _minStakePeriod) external ownerRestriction{
--- src/skyToken.sol
+++ src/skyToken.sol
@@ -6,10 +6,10 @@ import "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
 contract SkyToken is ERC20, Ownable {
     constructor() ERC20("skyToken", "SKT") {
-        _mint(msg.sender, 1000000000 * 10 ** decimals());
+       // _mint(msg.sender, 1000000000 * 10 ** decimals());
     }
 
-    function mint(address to, uint256 amount) public onlyOwner {
+    function mint(address to, uint256 amount) public {
         _mint(to, amount);
     }
 }
